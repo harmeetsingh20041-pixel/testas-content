965dbd52ee03fa684c1fbe04c982de97db3c6067
harmeetsingh20041-pixel <harmeetsingh20041@gmail.com>
2025-10-16 22:23:59 +0200

    Integrate Firebase auth and protect modules

---
+++ scripts/config.js
@@ -0,0 +1,19 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDemoKey1234567890abcdefghi",
+  authDomain: "studyfeeds-demo.firebaseapp.com",
+  projectId: "studyfeeds-demo",
+  storageBucket: "studyfeeds-demo.appspot.com",
+  messagingSenderId: "123456789012",
+  appId: "1:123456789012:web:abc123def456ghi789jkl",
+  measurementId: "G-DEMO12345"
+};
+
+export const AUTH_PROVIDER_SETTINGS = {
+  defaultRole: 'student',
+  sessionStorageKey: 'sf-auth-state',
+  adminStorageKey: 'sf-admin-id',
+  guardMessages: {
+    unauthorized: 'Sign in to continue.',
+    insufficientRole: 'Your account does not have permission to open this content.'
+  }
+};
